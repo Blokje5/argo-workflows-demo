aaa8009d5e011233541e9c209fd9292151a3fd77
Lennard Eijsackers <lennardeijsackers92@gmail.com>
2021-02-23 19:54:18 +0100

    Chore: refactor to Nginx controller as Traefik caused issues

--- 01_installation/install.sh
+++ 01_installation/install.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-helm repo add traefik https://helm.traefik.io/traefik && helm repo update && helm install --set ports.traefik.hostPort=9000,ports.web.hostPort=80 traefik traefik/traefik
-kubectl apply -f $DIR/traefik/dashboard.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s
 kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-workflows/stable/manifests/namespace-install.yaml
 kubectl apply -f $DIR/argo/ingress.yaml
 kubectl apply -f $DIR/argo/workflow-executor.yaml
+
